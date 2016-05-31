9cee783dc8972efaf3c218b73487aee07158a3f1
Lair Junior <ljunior@thoughtworks.com>
2016-05-31 02:34:22 -0300

    added derby and insert method

--- tecnicasTF/src/entities/FlightTicket.java
+++ tecnicasTF/src/entities/FlightTicket.java
@@ -6,4 +6,35 @@ public class FlightTicket {
 	private FlightPlan flight;
 	private FlightSeat selectedSeat;
 	private int Status;
+	
+	public int getLocaleNumber() {
+		return localeNumber;
+	}
+	public void setLocaleNumber(int localeNumber) {
+		this.localeNumber = localeNumber;
+	}
+	public Passenger getPassenger() {
+		return passenger;
+	}
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}
+	public FlightPlan getFlight() {
+		return flight;
+	}
+	public void setFlight(FlightPlan flight) {
+		this.flight = flight;
+	}
+	public FlightSeat getSelectedSeat() {
+		return selectedSeat;
+	}
+	public void setSelectedSeat(FlightSeat selectedSeat) {
+		this.selectedSeat = selectedSeat;
+	}
+	public int getStatus() {
+		return Status;
+	}
+	public void setStatus(int status) {
+		Status = status;
+	}
 }
---
+++ tecnicasTF/src/repositories/dao/IDatabase.java
@@ -0,0 +1,10 @@
+package repositories.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface IDatabase {
+
+	Connection getConnection() throws SQLException;
+
+}
