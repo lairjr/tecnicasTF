e3e7a483d7655e4c2767b1b3d59c4b186fc2f895
Lair Junior <lairjr@outlook.com>
2016-06-23 01:47:44 -0300

    adds ticket controller separetly

---
+++ src/view/controllers/TicketsController.java
@@ -0,0 +1,14 @@
+package view.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by ljunior on 6/23/16.
+ */
+public class TicketsController {
+    @FXML
+    private TextField departureFlightNumber;
+    @FXML
+    private TextField arrivalFlightNumber;
+}
