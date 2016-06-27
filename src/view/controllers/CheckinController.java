764972ab9c8d419164a7b8dee075d580698fb8ec
Lair Junior <lairjr@outlook.com>
2016-06-27 20:08:57 -0300

    fixes checkin

--- src/view/controllers/CheckinController.java
+++ src/view/controllers/CheckinController.java
@@ -95,10 +95,12 @@ public class CheckinController implements Initializable {
     }
 
     private void displayFlight(FlightDTO flight, List<CheckBox> checkboxes) {
-        for (SeatDTO seat: flight.getSeats()) {
-            CheckBox checkbox = checkboxes.get(seat.getNumber() - 1);
-            checkbox.setDisable(seat.getOccupied());
-            checkbox.setSelected(seat.getOccupied());
+        if (flight != null) {
+            for (SeatDTO seat: flight.getSeats()) {
+                CheckBox checkbox = checkboxes.get(seat.getNumber() - 1);
+                checkbox.setDisable(seat.getOccupied());
+                checkbox.setSelected(seat.getOccupied());
+            }
         }
     }
 
